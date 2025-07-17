1183c46307f3796faa69c3e4975eb5945b5c1213
ankulv <durgeshverma141@gmail.com>
2025-07-17 09:30:09 +0530

    gitprog

--- src/Project1/clasproggit.java
+++ src/Project1/clasproggit.java
@@ -3,8 +3,7 @@ package Project1;
 public class clasproggit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("printing for the git");
 	}
 
 }
